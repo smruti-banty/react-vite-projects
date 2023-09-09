b96522b33c0621945605cd52c8cf823b60b5bc36
smruti <srnayak165@gmail.com>
2023-09-09 06:06:22 +0530

    fix: More content add

--- text-expand/src/App.tsx
+++ text-expand/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <TextExpand intialDisplayLetter={2}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, suscipit nesciunt. Ducimus perferendis aut repellat vero est placeat harum? Esse eos fugiat itaque architecto quasi velit ipsum incidunt voluptate explicabo?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus cumque, sequi dicta, itaque officia repellat consequatur deleniti tenetur debitis odit cupiditate minima voluptate culpa hic, eius quasi suscipit doloribus quisquam!
       </TextExpand>
     </>
   );
